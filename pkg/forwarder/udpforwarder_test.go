8368c644e9549bec3b7a3783743ea2b009e1aa43
Leo <leonardjonathanoh@live.com>
2019-10-10 15:47:13 +0000

    Add pkg/forwarder/udpforwarder_test.go

---
+++ pkg/forwarder/udpforwarder_test.go
@@ -0,0 +1,19 @@
+package udpforwarder
+
+import (
+	"testing"
+
+)
+
+
+func TestForward(t *testing.T) {
+	forwarder, err := Forward(
+		"127.0.0.1:12345",
+		"1.1.1.1:12345",
+		DefaultTimeout,
+		"foo",
+	)
+	if forwarder == nil || err != nil {
+		t.Errorf("Failed to isntantiate Forwarder")
+	}
+}
