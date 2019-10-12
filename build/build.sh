52bcd3d8447fe1997a84b968ede91e65cfac5658
Leo <leonardjonathanoh@live.com>
2019-10-12 14:42:38 +0000

    Move main.go into cmd

--- build/build.sh
+++ build/build.sh
@@ -44,5 +44,5 @@ if [ -n "$INSTALL_GIT" ]; then
     apk add --no-cache git
 fi
 
-go build -o "$OUTBIN" -ldflags "-s -w -extldflags \"-static\" -X $(go list -m)/pkg/version.VERSION=$VERSION -X $(go list -m)/pkg/version.COMMIT_SHA1=$COMMIT_SHA1 -X $(go list -m)/pkg/version.BUILD_DATE=$BUILD_DATE"
+go build -o "$OUTBIN" -ldflags "-s -w -extldflags \"-static\" -X $(go list -m)/cmd.VERSION=$VERSION -X $(go list -m)/cmd.COMMIT_SHA1=$COMMIT_SHA1 -X $(go list -m)/cmd.BUILD_DATE=$BUILD_DATE"
 
---
+++ cmd/version.go
@@ -0,0 +1,33 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var (
+	// VERSION, BUILD_DATE, GIT_COMMIT are filled in by the build script
+	VERSION     = "<Will be added by go build>"
+	BUILD_DATE  = "<Will be added by go build>"
+	COMMIT_SHA1 = "<Will be added by go build>"
+)
+
+func getVersion() string {
+	return fmt.Sprintf("Version: %s, Commit SHA: %s, Build Date: %s, Go Version: %s", VERSION, COMMIT_SHA1, BUILD_DATE, runtime.Version())
+}
